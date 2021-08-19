b9c09971c7c1b24fe7b8bc8faad1cc08242958ca
Matheus Nogueira Reinaldo <74919381+MatheusNRei@users.noreply.github.com>
2021-08-19 14:47:01 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {Component,useState,useEffect} from "react";
 
 const  App = () => {
   //state
-  const[url,setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
+  const[url,setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react') // https://newsapi.org/v2/top-headlines?country=br&apiKey=API_KEY API Brasilieira
   const [news,setNews] = useState([]);
   const [searchQuery, setSearchQuery] = useState('react')
   const [loading,setLoading] = useState(false);
